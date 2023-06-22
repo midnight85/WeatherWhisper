926e2f57179b32f7f9d2f4a9e63502536575efae
midnight85 <maksvell2407@gmail.com>
2023-06-23 00:05:36 +0300

    create SunDetails UI component

---
+++ components/UI/Forecast/SunDetails.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import {Text, StyleSheet, View} from 'react-native';
+import {Sunset, Sunrise} from '../../Icons';
+import {COLORS, FONT_WEIGHT, TEXT} from '../../../constants/GlobalStyles';
+
+function SunDetails({value, status}) {
+  return (
+    <View style={styles.container}>
+      <View>
+        <Text style={styles.value}>{value}</Text>
+        <Text style={styles.text}>{status}</Text>
+      </View>
+      {status === 'sunrise' && (
+        <Sunrise
+          size={24}
+          color={COLORS.secondaryColor700}
+        />
+      )}
+      {status === 'sunset' && (
+        <Sunset
+          size={24}
+          color={COLORS.secondaryColor700}
+        />
+      )}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 16,
+    borderRadius: 16,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    backgroundColor: COLORS.secondaryColor_ct300_12,
+    aspectRatio: 2.22,
+  },
+  value: {
+    color: COLORS.secondaryColor900,
+    ...FONT_WEIGHT.semiBold,
+    ...TEXT.subT1,
+  },
+  text: {
+    color: COLORS.secondaryColor400,
+    ...FONT_WEIGHT.regular,
+    ...TEXT.caption,
+  },
+});
+
+export default SunDetails;
