7c5ce39dc517a8ea263f85c28d040349290a62dc
midnight85 <maksvell2407@gmail.com>
2023-07-01 01:41:51 +0300

    fix styles for HomeStack navigation header

--- screens/Home/HomeStack.js
+++ screens/Home/HomeStack.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import {StyleSheet} from 'react-native';
+import {StyleSheet, View} from 'react-native';
 import {createStackNavigator, TransitionPresets} from '@react-navigation/stack';
 import HomeScreen from './HomeScreen';
 import Forecast from './Forecast';
 import AqiDetails from './AqiDetails';
 import {AQI_DETAILS, FORECAST, HOME} from '../../constants/ScreenNames';
-import {FONT_WEIGHT, TEXT} from '../../constants/GlobalStyles';
+import {COLORS, FONT_WEIGHT, TEXT} from '../../constants/GlobalStyles';
+import {ArrowBack} from '../../components/Icons';
 
 const Stack = createStackNavigator();
 
@@ -15,10 +16,28 @@ function HomeStack() {
       // initialRouteName={AQI_DETAILS}
       screenOptions={{
         ...TransitionPresets.SlideFromRightIOS,
+        headerStyle: {
+          borderBottomWidth: 1,
+          borderBottomColor: COLORS.neutralColors200,
+        },
         headerTitleStyle: {
           ...FONT_WEIGHT.medium,
           ...TEXT.subT1,
         },
+        headerTitleContainerStyle: {
+          marginLeft: 0,
+          justifyContent: 'center',
+        },
+        headerTitleAlign: 'left',
+        headerLeftContainerStyle: {},
+        headerBackImage: ({tintColor}) => (
+          <View style={{marginHorizontal: 5, marginVertical: 9}}>
+            <ArrowBack
+              size={24}
+              color={tintColor}
+            />
+          </View>
+        ),
       }}>
       <Stack.Screen
         name={HOME}
