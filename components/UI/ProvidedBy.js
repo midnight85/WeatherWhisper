f50b4d8d4f4806453519ae883369c0261a4a1f7f
midnight85 <maksvell2407@gmail.com>
2023-07-01 22:55:41 +0300

    fix url open in ProvidedBy component

--- components/UI/ProvidedBy.js
+++ components/UI/ProvidedBy.js
@@ -4,15 +4,7 @@ import {COLORS, FONT_WEIGHT, TEXT} from '../../constants/GlobalStyles';
 
 function ProvidedBy({dataText, source, url, style}) {
   const handlePress = React.useCallback(async () => {
-    // Checking if the link is supported for links with custom URL scheme.
-    const supported = await Linking.canOpenURL(url);
-    if (supported) {
-      // Opening the link with some app, if the URL scheme is "http" the web link should be opened
-      // by some browser in the mobile
-      await Linking.openURL(url);
-    } else {
-      Alert.alert(`Don't know how to open this URL: ${url}`);
-    }
+    await Linking.openURL(url);
   }, [url]);
   return (
     <Text style={[styles.text, style && style]}>
